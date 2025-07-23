8187528637ad424f424d2f7c554845bfaa637073
Mateus Lima <mateuszinlima7@gmail.com>
2025-07-23 11:04:36 -0300

    Atualização de adaptação de tela mobile menor

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -8,11 +8,11 @@ function AboutMe() {
       <div className="about-content">
         <div className="about-text">
           <h2>Quem Somos</h2>
-          <p>
+          <p className="pr">
             2011 marcou o início da nossa missão: cuidar de pessoas com empatia, excelência e compromisso real com a saúde.
             Desde então, transformamos desafios em aprendizado e evoluímos com cada paciente que passou por aqui.
           </p>
-          <p>
+          <p className="pr">
             Ao longo de mais de uma década, investimos em conhecimento, tecnologia e, principalmente, em pessoas — profissionais dedicados que fazem da CAMESO uma referência em atendimento humanizado.
             Hoje, oferecemos mais do que consultas. Entregamos acolhimento, confiança e cuidado personalizado, em um ambiente que valoriza o bem-estar em cada detalhe.
             <br /><br />
